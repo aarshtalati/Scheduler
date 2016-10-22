5c0204a6dc7fbfffe967f09aacd7ba3f833262c8
Aarsh Talati <aarshstalati1989@gmail.com>
2016-10-21 21:48:22 -0400

    add angular integration with jade templates

---
+++ Scheduler/public/app/app.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module('app', ['ngResource', 'ngRoute']);
+
+app.config(['$routeProvider', '$locationProvider',
+    function ($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+        $routeProvider
+            .when('/', {
+                templateUrl: '/partials/main',
+                controller: 'mainController'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }
+]);
+
+app.controller('mainController', function ($scope) {
+    $scope.test = '123 test';
+});
--- Scheduler/server.js
+++ Scheduler/server.js
@@ -1,6 +1,43 @@
-﻿var http = require('http');
+﻿//var http = require('http');
+//var port = process.env.port || 1337;
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello World\n');
+//}).listen(port);
+
+var express = require('express');
+var stylus = require('stylus');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV | 'development';
+var app = express();
+
+function compile(str, path) {
+    return stylus(str).set('filename', path);
+}
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(stylus.middleware({
+    src: __dirname + '/public',
+    compile: compile
+}
+));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/partials/:partialPath', function (req, res) {
+    res.render('partials/' + req.params.partialPath);
+});
+
+// ideally these should match with ng-routes for favicon etc., as a good practice
+app.get('*', function (req, res) {
+    res.render('index');
+});
+
 var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+app.listen(port);
